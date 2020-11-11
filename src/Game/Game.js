d0b3f1d90c61fa483d9b811bb545955f8ef6b514
paul-antony <paulantonytoc@github.com>
2020-11-11 14:24:38 +0530

    added title to app

---
+++ src/Game/Game.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import './App.css';
+import Title from './Title/Title';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+          <Title/>
+      </div>
+    );
+    
+  }
+}
+
+export default Game;
---
+++ src/Game/Title/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Title.css';
+
+const title = () => {
+    return (
+    <div className="game--title" >Tudoku</div>
+    )
+};
+
+export default title;
